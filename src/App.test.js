c7f50de9fbb4912f16fce5b185d763d3aa85d9e4
Elvis <elvisgarcia93@gmail.com>
2023-02-17 12:58:46 -0500

    Update Test Case to Fail

--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,6 @@ it('should render', () => {
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
 
-// it('should be 4', () => {
-//   expect(3).toBe(4);
-// })
+it('should be 4', () => {
+  expect(3).toBe(4);
+})
